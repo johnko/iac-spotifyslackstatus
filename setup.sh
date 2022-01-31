2049509720f3dbde3c53c639c9b2f50a8a6d7629
johnko <279736+johnko@users.noreply.github.com>
2022-01-31 18:48:25 -0500

    generate default_tags in provider

--- setup.sh
+++ setup.sh
@@ -7,6 +7,15 @@ set -euo pipefail
 APP="tf-remote-backend"
 pushd $APP
 
+# replace variables
+TERRAFORM_VERSION="$( terraform -version | grep Terraform | cut -d' ' -f2 )"
+GIT_COMMIT="$( git rev-parse --short=7 HEAD )"
+cat provider.template \
+    | sed "s,APP_NAME,$APP," \
+    | sed "s,TERRAFORM_VERSION,$TERRAFORM_VERSION," \
+    | sed "s,GIT_COMMIT,$GIT_COMMIT," \
+    > provider.tf
+
 # deploy state bucket and statelock table using local state
 mv backend.tf ../backend.tf
 terraform init
