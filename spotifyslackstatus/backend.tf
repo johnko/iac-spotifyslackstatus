edac79c3f667cb310cae90e80c0c1797c82f6ef2
johnko <279736+johnko@users.noreply.github.com>
2022-02-01 00:43:36 -0500

    replace AWS_ACCOUNT_ID on the fly

--- spotifyslackstatus/backend.tf
+++ spotifyslackstatus/backend.tf
@@ -2,9 +2,9 @@ terraform {
   backend "s3" {
     key = "spotifyslackstatus/terraform.tfstate"
 
-    kms_key_id     = "arn:aws:kms:ca-central-1:341506437258:alias/aws/s3"
-    dynamodb_table = "statelock-341506437258"
-    bucket         = "statebucket-341506437258"
+    kms_key_id     = "arn:aws:kms:ca-central-1:AWS_ACCOUNT_ID:alias/aws/s3"
+    dynamodb_table = "statelock-AWS_ACCOUNT_ID"
+    bucket         = "statebucket-AWS_ACCOUNT_ID"
 
     encrypt = true
     region  = "ca-central-1"
