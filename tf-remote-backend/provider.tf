b5a54122a0754cf1649423808fe6e0e17652bd0a
johnko <279736+johnko@users.noreply.github.com>
2022-01-31 19:49:20 -0500

    store last used backend/provider

---
+++ tf-remote-backend/provider.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  default_tags {
+    tags = {
+      app                = "tf-remote-backend"
+      iacdeployer        = "terraform"
+      iacdeployerversion = "v1.1.4"  # Value from: terraform -version
+      iacgitcommit       = "e32435b" # Value from: git rev-parse --short=7 HEAD
+    }
+  }
+  region      = "ca-central-1"
+  max_retries = 5
+
+  skip_get_ec2_platforms  = true
+  skip_metadata_api_check = true
+}
