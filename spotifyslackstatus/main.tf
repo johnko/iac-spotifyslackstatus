367e482a066501e27f58fda7d9e402e84952e493
johnko <279736+johnko@users.noreply.github.com>
2022-02-03 02:16:31 -0500

    convert to module

--- spotifyslackstatus/main.tf
+++ spotifyslackstatus/main.tf
@@ -2,5 +2,29 @@ locals {
   app = "spotifyslackstatus"
 }
 
-# TODO convert SubscriptionFilter into module
-# TODO use SubscriptionFilter module to logfilter for apigw LogGroup
+module "hello" {
+  source = "./modules/lambda_api_route"
+
+  service_name = "hello"
+  lambda_handler = "index.hello"
+  lambda_runtime = "python3.8"
+  lambda_zip_file = "index.zip"
+  apigw_route_http_method = "GET"
+  apigw_route_http_path = "/hello"
+  app = local.app
+  iam_role_cw2fh_arn = aws_iam_role.role_cw2fh.arn
+  iam_role_fh2s3executelog_arn = aws_iam_role.role_fh2s3executelog.arn
+  iam_role_lambda_arn = aws_iam_role.role_lambda.arn
+  kms_key_arn = aws_kms_key.cmk_spotifyslackstatus.arn
+  logbucket = local.logbucket
+  apigw_id = aws_apigatewayv2_api.apigw.id
+  apigw_exec_arn = aws_apigatewayv2_api.apigw.execution_arn
+  session_dynamodb_table = aws_dynamodb_table.statelock_table.name
+  session_dynamodb_region = local.region
+  
+  depends_on = [
+    aws_iam_role_policy_attachment.attach_role_policy_lambda,
+    aws_iam_role_policy_attachment.attach_role_policy_fh2s3executelog,
+    aws_iam_role_policy_attachment.attach_role_policy_cw2fh,
+  ]
+}
---
+++ spotifyslackstatus/modules/lambda_api_route/lambda-api-route.tf
@@ -0,0 +1,115 @@
+locals {
+  app = var.app
+  loggroup_lambda_name = "/aws/lambda/${local.lambda_name}"
+  lambda_name         = "${local.app}-${var.service_name}"
+  loggroup_fh2s3_name  = "/aws/kinesisfirehose/${local.fh2s3_name}"
+  fh2s3_name          = "${aws_lambda_function.lambda.function_name}-fh2s3"
+  subfilter_cw2fh_name = "${local.lambda_name}-subfil"
+}
+
+##### Lambda LogGroup
+resource "aws_cloudwatch_log_group" "loggroup_lambda" {
+  name              = local.loggroup_lambda_name
+  retention_in_days = 90
+  kms_key_id        = var.kms_key_arn
+  tags = {
+    Name               = local.loggroup_lambda_name
+    dataclassification = "restricted"
+  }
+}
+####################
+##### Lambda
+resource "aws_lambda_function" "lambda" {
+  function_name = local.lambda_name
+  role          = var.iam_role_lambda_arn
+  handler       = var.lambda_handler
+  filename      = var.lambda_zip_file
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256(var.lambda_zip_file)
+  runtime          = var.lambda_runtime
+  kms_key_arn      = var.kms_key_arn # comment this out if you want to use AWS managed key
+  environment {
+    variables = {
+      SESSION_DYNAMODB_REGION = var.session_dynamodb_region,
+      SESSION_DYNAMODB_TABLE  = var.session_dynamodb_table,
+    }
+  }
+  tags = {
+    Name               = local.lambda_name
+    dataclassification = "public"
+  }
+  depends_on = [
+    aws_cloudwatch_log_group.loggroup_lambda,
+  ]
+}
+
+####################
+##### Firehose LogGroup
+resource "aws_cloudwatch_log_group" "loggroup_fh2s3" {
+  name              = local.loggroup_fh2s3_name
+  retention_in_days = 90
+  kms_key_id        = var.kms_key_arn
+  tags = {
+    Name               = local.loggroup_fh2s3_name
+    dataclassification = "restricted"
+  }
+}
+##### Firehose repeat for each lambda
+resource "aws_kinesis_firehose_delivery_stream" "fh2s3" {
+  name        = local.fh2s3_name
+  destination = "extended_s3"
+  server_side_encryption {
+    enabled  = true
+    key_type = "CUSTOMER_MANAGED_CMK"                 # or AWS_OWNED_CMK
+    key_arn  = var.kms_key_arn # comment this out if you want to use AWS_OWNED_CMK
+  }
+  extended_s3_configuration {
+    role_arn           = var.iam_role_fh2s3executelog_arn
+    bucket_arn         = "arn:aws:s3:::${var.logbucket}"
+    prefix             = "executelogs/lambda/${aws_lambda_function.lambda.function_name}/"
+    compression_format = "GZIP"
+    # kms_key_arn not used since logbucket is SSE-S3 / AES256
+    cloudwatch_logging_options {
+      enabled         = true
+      log_group_name  = aws_cloudwatch_log_group.loggroup_fh2s3.name
+      log_stream_name = "logstream"
+    }
+  }
+  tags = {
+    Name               = local.fh2s3_name
+    dataclassification = "restricted"
+  }
+}
+##### CloudWatch SubscriptionFilter forwards logs to firehose to bucket
+resource "aws_cloudwatch_log_subscription_filter" "subfilter_cw2fh" {
+  name           = local.subfilter_cw2fh_name
+  role_arn       = var.iam_role_cw2fh_arn
+  log_group_name = aws_cloudwatch_log_group.loggroup_lambda.name
+  # https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/FilterAndPatternSyntax.html
+  filter_pattern  = " " # all events
+  destination_arn = aws_kinesis_firehose_delivery_stream.fh2s3.arn
+}
+
+####################
+##### Lambda + APIGW integration
+resource "aws_apigatewayv2_route" "route_lambda" {
+  api_id    = var.apigw_id
+  route_key = "${var.apigw_route_http_method} ${var.apigw_route_http_path}"
+  target    = "integrations/${aws_apigatewayv2_integration.integration_lambda.id}"
+}
+resource "aws_apigatewayv2_integration" "integration_lambda" {
+  api_id             = var.apigw_id
+  integration_uri    = aws_lambda_function.lambda.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+##### Lambda Permission
+resource "aws_lambda_permission" "perm_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${var.apigw_exec_arn}/*/*"
+}
