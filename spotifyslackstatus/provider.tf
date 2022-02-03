f9f052f547930138e6c2dd5e3623e64a65c14bb3
johnko <279736+johnko@users.noreply.github.com>
2022-02-02 23:12:54 -0500

    save last provider

--- spotifyslackstatus/provider.tf
+++ spotifyslackstatus/provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
       app                = "spotifyslackstatus"
       iacdeployer        = "terraform"
       iacdeployerversion = "v1.1.4"  # Value from: terraform -version
-      iacgitcommit       = "82c4649" # Value from: git rev-parse --short=7 HEAD
+      iacgitcommit       = "cfee29a" # Value from: git rev-parse --short=7 HEAD
     }
   }
   region      = "ca-central-1"
