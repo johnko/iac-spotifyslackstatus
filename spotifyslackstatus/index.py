b0264188bcd4e11349f16d29569210e66cb9bf4e
johnko <279736+johnko@users.noreply.github.com>
2022-02-03 02:33:56 -0500

    add goodbye and TODO

--- spotifyslackstatus/index.py
+++ spotifyslackstatus/index.py
@@ -22,3 +22,21 @@ def hello(event, context):
         "body": json.dumps(responseBody),
     }
     return response
+
+def goodbye(event, context):
+    # logger.info("## ENVIRONMENT VARIABLES\n" + json.dumps(dict(**os.environ)))
+    logger.info("## EVENT\n" + json.dumps(event))
+
+    responseCode = 200
+    responseBody = {
+        "message": "Goodbye"
+    }
+
+    response = {
+        "statusCode": responseCode,
+        "headers": {
+            "x-custom-header" : "my custom header value"
+        },
+        "body": json.dumps(responseBody),
+    }
+    return response
--- spotifyslackstatus/main.tf
+++ spotifyslackstatus/main.tf
@@ -29,3 +29,33 @@ module "hello" {
     aws_iam_role_policy_attachment.attach_role_policy_cw2fh,
   ]
 }
+
+module "goodbye" {
+  source = "./modules/lambda_api_route"
+
+  service_name            = "goodbye"
+  lambda_handler          = "index.goodbye"
+  lambda_runtime          = "python3.8"
+  lambda_zip_file         = "index.zip"
+  apigw_route_http_method = "GET"
+  apigw_route_http_path   = "/goodbye"
+
+  app                          = local.app
+  iam_role_cw2fh_arn           = aws_iam_role.role_cw2fh.arn
+  iam_role_fh2s3executelog_arn = aws_iam_role.role_fh2s3executelog.arn
+  iam_role_lambda_arn          = aws_iam_role.role_lambda.arn
+  kms_key_arn                  = aws_kms_key.cmk_spotifyslackstatus.arn
+  logbucket                    = local.logbucket
+  apigw_id                     = aws_apigatewayv2_api.apigw.id
+  apigw_exec_arn               = aws_apigatewayv2_api.apigw.execution_arn
+  session_dynamodb_table       = aws_dynamodb_table.statelock_table.name
+  session_dynamodb_region      = local.region
+
+  depends_on = [
+    aws_iam_role_policy_attachment.attach_role_policy_lambda,
+    aws_iam_role_policy_attachment.attach_role_policy_fh2s3executelog,
+    aws_iam_role_policy_attachment.attach_role_policy_cw2fh,
+  ]
+}
+
+# TODO convert to Lambda Layer to reduce Code storage
