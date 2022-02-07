e3f60d01d62bac8f98b1f0271a9107f32e90290c
johnko <279736+johnko@users.noreply.github.com>
2022-02-06 22:10:52 -0500

    update goodbye example

--- spotifyslackstatus/main.tf
+++ spotifyslackstatus/main.tf
@@ -44,34 +44,32 @@ module "hello" {
   ]
 }
 
-# module "goodbye" {
-#   source = "./modules/lambda_api_route"
+module "goodbye" {
+  source = "./modules/lambda_api_route"
 
-#   service_name   = "goodbye"
-#   lambda_handler = "index.goodbye"
-#   lambda_runtime = "python3.8"
-#   # lambda_zip_file         = "index.zip"
-#   lambda_s3_bucket = aws_s3_bucket.lambdabucket.id
-#   lambda_s3_object = aws_s3_bucket_object.lambdaobject.id
-#   apigw_route_http_method = "GET"
-#   apigw_route_http_path   = "/goodbye"
+  service_name   = "goodbye"
+  lambda_handler = "index.goodbye"
+  lambda_runtime = "python3.8"
+  # lambda_zip_file         = "index.zip"
+  lambda_s3_bucket = aws_s3_bucket.lambdabucket.id
+  lambda_s3_object = aws_s3_bucket_object.lambdaobject.id
+  apigw_route_http_method = "GET"
+  apigw_route_http_path   = "/goodbye"
 
-#   app                          = local.app
-#   iam_role_cw2fh_arn           = aws_iam_role.role_cw2fh.arn
-#   iam_role_fh2s3executelog_arn = aws_iam_role.role_fh2s3executelog.arn
-#   iam_role_lambda_arn          = aws_iam_role.role_lambda.arn
-#   kms_key_arn                  = aws_kms_key.cmk_spotifyslackstatus.arn
-#   logbucket                    = local.logbucket
-#   apigw_id                     = aws_apigatewayv2_api.apigw.id
-#   apigw_exec_arn               = aws_apigatewayv2_api.apigw.execution_arn
-#   session_dynamodb_table       = aws_dynamodb_table.statelock_table.name
-#   session_dynamodb_region      = local.region
+  app                          = local.app
+  iam_role_cw2fh_arn           = aws_iam_role.role_cw2fh.arn
+  iam_role_fh2s3executelog_arn = aws_iam_role.role_fh2s3executelog.arn
+  iam_role_lambda_arn          = aws_iam_role.role_lambda.arn
+  kms_key_arn                  = aws_kms_key.cmk_spotifyslackstatus.arn
+  logbucket                    = local.logbucket
+  apigw_id                     = aws_apigatewayv2_api.apigw.id
+  apigw_exec_arn               = aws_apigatewayv2_api.apigw.execution_arn
+  session_dynamodb_table       = aws_dynamodb_table.statelock_table.name
+  session_dynamodb_region      = local.region
 
-#   depends_on = [
-#     aws_iam_role_policy_attachment.attach_role_policy_lambda,
-#     aws_iam_role_policy_attachment.attach_role_policy_fh2s3executelog,
-#     aws_iam_role_policy_attachment.attach_role_policy_cw2fh,
-#   ]
-# }
-
-# TODO convert to Lambda Layer to reduce Code storage
+  depends_on = [
+    aws_iam_role_policy_attachment.attach_role_policy_lambda,
+    aws_iam_role_policy_attachment.attach_role_policy_fh2s3executelog,
+    aws_iam_role_policy_attachment.attach_role_policy_cw2fh,
+  ]
+}
